d872839eb31439abc641f71c605d589545005e73
sofiabirrer <sofia.birrer@gmail.com>
2018-01-31 22:07:07 +0100

    Initial commit.

---
+++ git/git_commands.sh
@@ -0,0 +1,12 @@
+git init #startbefehl für git
+git status #in welchem Zustand der Ordner ist
+git add <Filename> #staged (Bühne) das File
+git commit #speichert Files auf stage
+git diff <Filename> #shows differences
+git log #whole history of the repository
+repository #Ort an welchem alles gespeichert wird
+git checkout <branch|commit> # anschauen/ go to )
+branch # ast
+git branch # shows all branches
+git checkout exercices # Hausaufgaben
+
---
+++ shell/commands.sh
@@ -0,0 +1,17 @@
+ls .               # Show contents of the current directory
+cd <dir>           # Change Directory, meaning change
+mkdir <dirname>    # Create new directory
+touch <filename>   # Create new file
+cd ..              # Go to one directory upwards in the tree.
+rm <file>          # Removes/deletes the given file
+rmdir <dirname>    # Removes an empty directory
+rm -r <dirname>	   # Removes a non-empty directory
+echo <message>     # Prints message
+cat  <filename>    # Prints contents of files
+
+sudo apt  install <packagename>  #installs package
+which <command>   # prints location of the command
+#ctrl + c = abbrechen
+#ctrl + d = exit 
+
+
